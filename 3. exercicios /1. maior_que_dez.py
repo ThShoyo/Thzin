bd8c59e3ed4f762d572340e57e1a7015cd6c2aa3
ThShoyo <Thzinshoyo@gmail.com>
2025-02-19 17:40:37 +0000

    salvar

---
+++ 3. exercicios /1. maior_que_dez.py
@@ -0,0 +1,8 @@
+# Limpar o terminal.
+import os
+os.system("clear")
+
+# Elabore um algoritimo para solicitar ao usuário um valor e 
+# escrever a mensagem: É MAIOR QUE 10! 
+# Se o valor lido for maior que 10, caso contrário escrever:  
+# NÃO É MAIOR QUE 10!
---
+++ 3. exercicios /Maior.py
@@ -0,0 +1,14 @@
+import os 
+os.system("clear")
+
+
+
+
+# Solicitar ao usuário um valor
+valor = int(input("Digite um valor"))
+
+# Verifica se o valor é ,ar maior que 10
+if valor > 10: 
+    print(""É MAIOR QUE 10 !)
+else: 
+print("nÃO É MAIOR QUE 10"!)
