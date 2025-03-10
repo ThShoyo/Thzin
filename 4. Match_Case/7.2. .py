c16aa885a0360a83c9487401fe5f7bcf59614e4f
ThShoyo <Thzinshoyo@gmail.com>
2025-03-10 18:32:40 +0000

    salvar

---
+++ 4. Match_Case/7.2. .py
@@ -0,0 +1,18 @@
+# Limpar o terminal.
+import os
+os.system ("clear")
+
+# Entrada
+altura = float(input("Digite sua altura: "))
+sexo = input("Digite seu sexo: ")
+
+# Processamneto
+match sexo:
+    case  "M" | "m":
+        peso_ideal = (72.7 *altura) - 58
+        print(f"\nPeso ideal: {peso_ideal}")
+    case  "F" | "m":
+        peso_ideal = (62.1 * altura) - 44.7
+        print(f"nPeso ideal: {peso_ideal}")
+    case _: 
+        print("Opcao inválida")
