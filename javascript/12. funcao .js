83e2fb16f246f53febf746376dec787875f77f5e
ThShoyo <Thzinshoyo@gmail.com>
2025-05-26 16:55:37 -0300

    Add files via upload

---
+++ javascript/12. funcao .js
@@ -0,0 +1,29 @@
+// Criando uma função.
+function somar (a, b) {
+        return a + b
+}
+
+function subtrair (a, b) {
+    return a - b
+}
+
+function dividir (a, b) {
+    return a / b
+}
+
+function multiplicar (a, b) {
+    return a * b
+}
+
+// Chamando  a função 
+// Adicionar o resultado da função na constante.
+const soma = somar(2,3)
+const subtracao = subtrair(2,3)
+const divisao = dividir(2,3)
+const multiplicao = multiplicar(2,3)
+
+// Mostra o conteúdo da cosntante.
+console.log(`Soma: ${soma}`)
+console.log(`Subtração: ${subtracao}`)
+console.log(`Divisão: ${divisao}`)
+console.log(`Multiplicação: ${multiplicao}`)
---
+++ javascript/7.laco_repeticao_for.js
@@ -0,0 +1,5 @@
+// LACO DE REPETICAO DOR DE 1 ATE 5
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
