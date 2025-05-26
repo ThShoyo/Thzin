2f78692526992c4c623e2ff20ed7eedd7c8a0cc3
ThShoyo <Thzinshoyo@gmail.com>
2025-05-26 18:02:30 +0000

    salvar

---
+++ Aula 1 HTML/javascript/1.variaiveis.js
@@ -0,0 +1,6 @@
+// os comandos 'var' e 'let' servem para criar variaveis
+let nome = 'Marta'
+var sexo = "feminino"
+// exibindo
+console.log('Nome: ', nome)
+console.log (`Sexo: ${sexo}`)
---
+++ Aula 1 HTML/javascript/2.constantes.js
@@ -0,0 +1,9 @@
+// constantes
+const idade = 20
+
+// Tentando mudar a idade
+// idade = 1 / Constante não muda
+
+// exibindo
+console.log('Idade: ', idade)
+console.log (`minha idade: ${idade}`)
